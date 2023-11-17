0ed71d79ddcef3897dd5cbceb012664eee4cfe51
ozgessolmaz <111462184+ozgessolmaz@users.noreply.github.com>
2023-11-17 12:10:16 +0300

    SinemaClass

---
+++ ConsoleApp3/operatorasiriyükleme.cs
@@ -0,0 +1,49 @@
+﻿using System ;
+
+namespace operatorAsiriYukleme
+{
+    class Sayi
+    {
+        public int a;
+        private int b;
+        protected int c = 250;
+
+        public int toplamHesapla()
+        {
+            return this.a + c;
+        }
+    }
+
+    class Deger : Sayi
+    {
+        public int m;
+        protected int n;
+
+        public int toplamBul()
+        {
+            return base.a + this.m + c;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sayi x = new Sayi();
+            Deger y = new Deger();
+            
+            Console.WriteLine(x.a);
+            Console.WriteLine(y.a);
+            Console.WriteLine(y.m);
+
+            y.a = 100;
+            y.m = 200;
+            
+            Console.WriteLine(y.toplamBul());
+            Console.WriteLine(y.toplamHesapla());
+
+            Console.ReadKey();
+
+        }
+    }
+}
